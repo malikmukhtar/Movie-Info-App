3c3ef587baebd250b78b974e9eb0f67448bd4943
Malik <malikmukhtar63@gmail.com>
2020-04-24 23:04:39 +0100

    final calls

--- main.js
+++ main.js
@@ -8,17 +8,18 @@ $(document).ready(() => {
 
 function getMovies(searchText) {
   axios
-    .get("http://www.omdbapi.com?s=" + searchText)
+    .get("http://www.omdbapi.com?s=" + searchText + "&apikey=8088643a")
     .then((response) => {
+      //   console.log(response);
       let movies = response.data.Search;
       let output = "";
       $.each(movies, (index, movie) => {
         output += `
-            <div class='col-md-3'>
+            <div class='col-md-3 mb-3'>
                 <div class='well text-center'>
                 <img src='${movie.Poster}'>
                 <h5>${movie.Title}</h5>
-                <a onclick='movieSelected('${movie.imdbID}')' class='btn btn-primary' href='#'>Movie Details</a>
+                <a onclick='movieSelected('${movie.imdbID}')' class='btn btn-primary' href='http://imdb.com/title/${movie.imdbID}'>Movie Details</a>
                 </div>
             </div>
             `;
